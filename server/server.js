a87b686751aab0498def916a64975837273e1efb
Faiqa Qayyum <faiqaqayyum4@gmail.com>
2026-06-07 21:19:35 +0500

    fix railway port issue

--- server/server.js
+++ server/server.js
@@ -84,9 +84,6 @@ async function realAI(msg) {
 
 // CHAT ROUTE
 app.post("/chat", async (req, res) => {
-  app.get("/", (req, res) => {
-  res.send("Backend is running 🚀");
-});
   try {
     const message = req.body.message;
 
@@ -143,6 +140,8 @@ app.post("/saveDiet", async (req, res) => {
 });
 
 // START SERVER
-app.listen(5000, () => {
-  console.log("🚀 Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
